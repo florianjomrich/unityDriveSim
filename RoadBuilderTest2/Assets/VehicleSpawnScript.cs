667809bd2b25bcaa92a7020e629e5376d2793c97
Florian Jomrich <florian.jomrich@gmail.com>
2015-07-06 13:00:06 +0200

    unity Spheren Update

---
+++ RoadBuilderTest2/Assets/VehicleSpawnScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.position = Random.insideUnitSphere * 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
